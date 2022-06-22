123d1fabce517109822244f7ae3a9a1921fa52ad
Milos <ostojicmilos10@gmail.com>
2022-06-22 10:06:57 -0400

    Add Contact edit functionality

--- src/components/Main/Profile.js
+++ src/components/Main/Profile.js
@@ -34,20 +34,18 @@ class Profile extends Component {
     
 
     render() {
-        const curr = this;
         const {description, hover, edit} = this.state;
 
         return (
             <div className="profile-section" onMouseOver={this.handleMouseIn} onMouseOut={this.handleMouseOut}>
                 <h4 className="section-title">Profile</h4>
                 <p>{description ? description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quaerat inventore accusamus autem amet. Quibusdam sed veniam provident doloremque soluta nisi itaque pariatur non vitae sint. Quae ea dolores facilis."}</p>
-                <button className={"edit-btn " + (hover ? "active" : "inactive")} onClick={this.toggleForm}>EDIT</button>
                 
-
+                {hover && <button className="edit-btn" onClick={this.toggleForm}>EDIT</button>}
                 {edit &&
-                <form action="" className={"edit-form"}>
+                <form action="" className="edit-form">
                     <div className="form-entry">
-                        <label htmlFor="description" className="form-label">Description</label>
+                        <label htmlFor="address" className="form-label">Description</label>
                         <textarea id="description" onChange={this.handleChange}/>
                     </div>
                     
--- src/components/SIdebar/Contact.js
+++ src/components/SIdebar/Contact.js
@@ -11,37 +11,102 @@ class Contact extends Component {
         super(props);
 
         this.state = {
-            address: '123 Abc Street, Toronto, ON',
-            email: 'yourname@mail.com',
-            phoneNum: '5033229513',
-            site: 'www.yoursite.com',
-        }
+            address: "",
+            email: "",
+            phoneNum: "",
+            site: "",
+            hover: false,
+            edit: false,
+        };
+    }
+
+    handleMouseIn = () => {
+        this.setState({hover: true});
+    }
+
+    handleMouseOut = () => {
+        this.setState({hover: false});
+    }
+
+    toggleForm = () => {
+        this.setState(prevState => ({
+            edit: !prevState.edit,
+            hover: !prevState.hover,
+        }));
+    }
+
+    handleAddressChange = (e) => {
+        this.setState({
+            address: e.target.value,
+        });
+    }
+
+    handleEmailChange = (e) => {
+        this.setState({
+            email: e.target.value,
+        });
+    }
+
+    handlePhoneChange = (e) => {
+        this.setState({
+            phone: e.target.value,
+        });
+    }
+
+    handleSiteChange = (e) => {
+        this.setState({
+            site: e.target.value,
+        });
     }
 
     render () {
-        const { address, email, phoneNum, site } = this.state;
+        const { address, email, phoneNum, site, hover, edit } = this.state;
 
         return (
-            <div className="sidebar-section">
+            <div className="sidebar-section" onMouseOver={this.handleMouseIn} onMouseOut={this.handleMouseOut}>
                 <h4 className="section-title">CONTACT</h4>
                 <ul className="section-info">
                     <li className="contact-item">
                         <img src={homeIcon} alt="Home" />
-                        <p>{address}</p>
+                        <p>{address ? address : "123 Abc Street, City, Country"}</p>
                     </li>
                     <li className="contact-item">
                         <img src={mailIcon} alt="Email" />
-                        <p>{email}</p>
+                        <p>{email ? email: "yourname@mail.com"}</p>
                     </li>
                     <li className="contact-item">
                         <img src={phoneIcon} alt="Phone" />
-                        <p>{phoneNum}</p>
+                        <p>{phoneNum ? phoneNum : "123-456-7899"}</p>
                     </li>
                     <li className="contact-item">
                         <img src={mapIcon} alt="Website" />
-                        <p>{site}</p>
+                        <p>{site ? site : "www.yoursite.com"}</p>
                     </li>
                 </ul>
+                {hover && <button className="edit-btn" onClick={this.toggleForm}>EDIT</button>}
+                {edit && 
+                    <form action="" className="edit-form">
+                        <div className="form-entry">
+                            <label htmlFor="address" className="form-label">Address</label>
+                            <input type="text" id="address" onChange={this.handleAddressChange}/>
+                        </div>
+                        <div className="form-entry">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="text" id="email" onChange={this.handleEmailChange}/>
+                        </div>
+                        <div className="form-entry">
+                            <label htmlFor="phone" className="form-label">Phone</label>
+                            <input type="text" id="phone" onChange={this.handlePhoneChange}/>
+                        </div>
+                        <div className="form-entry">
+                            <label htmlFor="website" className="form-label">Website</label>
+                            <input type="text" id="website" onChange={this.handleSiteChange}/>
+                        </div>
+                        
+                        <button className="close-btn" onClick={this.toggleForm}>Close</button>
+                    </form>
+                }
+
             </div>
         );
     }
